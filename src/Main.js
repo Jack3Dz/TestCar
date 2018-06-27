45d7d75634dda5265714ffb0083afd0f4f510570
Jefferson Damaceno <jefferson.damaceno17@gmail.com>
2018-06-26 23:41:26 -0300

    35%

--- src/Main.js
+++ src/Main.js
@@ -1,84 +1,69 @@
 // Main.js
 import React from 'react'
-import { FlatList, StyleSheet, Platform, Image, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Platform, Image, Text, View, ListView, StatusBar } from 'react-native'
 import firebase from 'react-native-firebase'
 import _ from 'lodash'; // 4.17.4
 import { ListItem } from 'react-native-elements';
 export default class Main extends React.Component {
-  state = { 
-    currentUser: null,
-    loading: false,
-    data: []
-  };
-
-  componentWillMount() {
-    firebase.database().ref('list').on('value', (data) => {
-      console.log(data.toJSON())
-    });
-
-    setTimeout(() => {
-      firebase.database().ref('list/002').set(
-        {
-          nome: 'Sentra',
-          preco: '90000'
-        }).then(() => {
-          console.log('Inserted !');
-        }).catch((error) => {
-          console.log(error);
-        })
-    }, 5000);
-    this.fetchData();
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      })
+    };
+    this.itemsRef = this.getRef().child('list');
   }
 
-  fetchData = async () => {
-    const response = await fetch("https://testcar-579d3.firebaseio.com/list.json");
-    const json = await response.json();
-    this.setState({ data: json.response });
+  getRef() {
+    return firebase.database().ref('/list');
+  }
+
+  listenForItems(itemsRef) {
+    itemsRef.on('value', (snap) => {
+
+      // get children as an array
+      var items = [];
+      snap.forEach((child) => {
+        items.push({
+          nome: child.val().nome,
+          preco: child.val().preco,
+          _key: child.key
+        });
+      });
+
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(items)
+      });
+
+    });
   }
 
   componentDidMount() {
-    // const { currentUser } = firebase.auth()
-    this.setState({ currentUser })
-    const currentUser = firebase.auth().currentUser.uid;
-    firebase.database().ref(`list/`)
-      .on('value', snapshot => {
-        const data = _.map(snapshot.val(), (uid) => {
-          return {uid}
-        });
-        this.setState({data, loading: false})
-      })
-  }
-
-  renderItem({item}) {
-    return (
-        <ListItem item={item} />
-      )
+    this.listenForItems(this.itemsRef);
   }
 
   render() {
-    const { currentUser } = this.state
-    if (this.state.loading) {
-      return (
-        <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
-          <ActivityIndicator size="large" color="dodgerblue" />
-        </View>
-      )
-    }
     return (
-      <View style={styles.container}>
-        <FlatList
-          data={this.state.data}
-          renderItem={this.renderItem}
-          keyExtractor={item => item.nome}
-        />
-      </View>
+      <View style={StyleSheet.container}>
+
+        <StatusBar title="Dictionary" />
+
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this._renderItem.bind(this)}
+          enableEmptySections={true}
+          style={StyleSheet.listview}/>
+
+
+          </View>
+        )
+      }
+  _renderItem(item) {
+
+    return (
+      <ListItem item={item}/>
     );
   }
+
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
