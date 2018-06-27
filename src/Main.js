26cf3fb67add466c0e48baf28d01fdb30653cac2
unknown <jefferson.damaceno17@gmail.com>
2018-06-27 12:55:48 -0300

    50%

--- src/Main.js
+++ src/Main.js
@@ -1,69 +1,104 @@
-// Main.js
-import React from 'react'
-import { FlatList, StyleSheet, Platform, Image, Text, View, ListView, StatusBar } from 'react-native'
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ListView,
+  ToolbarAndroid
+} from 'react-native';
 import firebase from 'react-native-firebase'
-import _ from 'lodash'; // 4.17.4
-import { ListItem } from 'react-native-elements';
+import styles from '../styles.js'
+import ListItem from '../components/ListItem.js';
+import FloatingActionButton from 'react-native-action-button';
 export default class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      dataSource: new ListView.DataSource({
-        rowHasChanged: (row1, row2) => row1 !== row2,
-      })
-    };
-    this.itemsRef = this.getRef().child('list');
-  }
-
-  getRef() {
-    return firebase.database().ref('/list');
-  }
-
-  listenForItems(itemsRef) {
-    itemsRef.on('value', (snap) => {
-
-      // get children as an array
-      var items = [];
-      snap.forEach((child) => {
-        items.push({
-          nome: child.val().nome,
-          preco: child.val().preco,
-          _key: child.key
-        });
-      });
-
-      this.setState({
-        dataSource: this.state.dataSource.cloneWithRows(items)
-      });
-
+    this.tasksRef = firebase.database().ref();
+    // Each list must has a dataSource, to set that data for it you must call: cloneWithRows()
+    // Check out the docs on the React Native List View here:
+    // https://facebook.github.io/react-native/docs/listview.html
+    const dataSource = new ListView.DataSource({
+      rowHasChanged: (row1, row2) => row1 !== row2,
     });
+    this.state = {
+      dataSource: dataSource, // dataSource for our list
+      newTask: "" // The name of the new task
+    };
   }
 
   componentDidMount() {
-    this.listenForItems(this.itemsRef);
+    // start listening for firebase updates
+    this.listenForTasks(this.tasksRef);
   }
 
   render() {
     return (
-      <View style={StyleSheet.container}>
-
-        <StatusBar title="Dictionary" />
-
+      <View style={styles.container}>
+        <ToolbarAndroid
+          style={styles.navbar}
+          title="Todo List" />
+        {/*A list view with our dataSource and a method to render each row*/}
+        {/*Allows lists to be empty, can be removed in future versions of react*/}
         <ListView
           dataSource={this.state.dataSource}
-          renderRow={this._renderItem.bind(this)}
           enableEmptySections={true}
-          style={StyleSheet.listview}/>
-
-
-          </View>
-        )
-      }
-  _renderItem(item) {
-
-    return (
-      <ListItem item={item}/>
+          renderRow={this._renderItem.bind(this)}
+          style={styles.listView} />
+        <TextInput
+          value={this.state.newTask}
+          style={styles.textEdit}
+          onChangeText={(text) => this.setState({ newTask: text })}
+          placeholder="New Task"
+        />
+        {/*The library has a bug so I removing the shadow to avoid it*/}
+        <FloatingActionButton
+          hideShadow={true}
+          buttonColor="rgba(231,76,60,1)"
+          onPress={this._addTask.bind(this)} />
+      </View>
     );
   }
 
+  _renderItem(task) {
+    // a method for building each list item
+    const onTaskCompletion = () => {
+      // removes the item from the list
+      this.tasksRef.child(task._key).remove()
+    };
+    return (
+      <ListItem task={task} onTaskCompletion={onTaskCompletion} />
+    );
+  }
+
+  _addTask() {
+    if (this.state.newTask === "") {
+      return;
+    }
+    this.tasksRef.push({ name: this.state.newTask });
+    this.setState({ newTask: "" });
+  }
+
+  listenForTasks(tasksRef) {
+    // listen for changes to the tasks reference, when it updates we'll get a
+    // dataSnapshot from firebase
+    tasksRef.on('value', (dataSnapshot) => {
+      // transform the children to an array
+      var tasks = [];
+      dataSnapshot.forEach((child) => {
+        tasks.push({
+          name: child.val().name,
+          _key: child.key
+        });
+      });
+
+      // Update the state with the new tasks
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(tasks)
+      });
+    });
+  }
 }
+
+AppRegistry.registerComponent('Main', () => Main);
